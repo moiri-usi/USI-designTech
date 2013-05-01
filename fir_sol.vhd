dd75d50d54fce2ea73ab205ba71e43dae4172c10
Simon Maurer <moirelein@gmail.com>
2013-05-01 19:07:36 +0200

    first add, then shift

--- fir_sol.vhd
+++ fir_sol.vhd
@@ -23,88 +23,108 @@ architecture Structural of fir_sol is
            O : out  STD_LOGIC_VECTOR (31 downto 0));
 	end component;
 
-	signal s1out : array32_t(0 to 9);
-	signal s2out : array32_t(0 to 9);
-	signal a1out : array32_t(0 to 9);
-	signal a2out : array32_t(0 to 9);
+	signal s1out : array32_t(0 to 4);
+	signal s2out : array32_t(0 to 4);
+	signal a1out : array32_t(0 to 4);
+	signal a2out : array32_t(0 to 4);
+	signal a3out : array32_t(0 to 4);
 	signal aout : array32_t(0 to 8);
 	
 begin
-    s1out(0) <= to_stdLogicVector(to_bitVector(Input(0)) sll 1);
-    s1out(9) <= to_stdLogicVector(to_bitVector(Input(9)) sll 1);
-    s1out(1) <= to_stdLogicVector(to_bitVector(Input(1)) sll 4);
-    s2out(1) <= to_stdLogicVector(to_bitVector(Input(1)) sll 3);
-    s1out(8) <= to_stdLogicVector(to_bitVector(Input(8)) sll 4);
-    s2out(8) <= to_stdLogicVector(to_bitVector(Input(8)) sll 3);
-    s1out(2) <= to_stdLogicVector(to_bitVector(Input(2)) sll 6);
-    s2out(2) <= to_stdLogicVector(to_bitVector(Input(2)) sll 5);
-    s1out(7) <= to_stdLogicVector(to_bitVector(Input(7)) sll 6);
-    s2out(7) <= to_stdLogicVector(to_bitVector(Input(7)) sll 5);
-    s1out(3) <= to_stdLogicVector(to_bitVector(Input(3)) sll 8);
-    s2out(3) <= to_stdLogicVector(to_bitVector(Input(3)) sll 7);
-    s1out(6) <= to_stdLogicVector(to_bitVector(Input(6)) sll 8);
-    s2out(6) <= to_stdLogicVector(to_bitVector(Input(6)) sll 7);
-    s1out(4) <= to_stdLogicVector(to_bitVector(Input(4)) sll 10);
-    s2out(4) <= to_stdLogicVector(to_bitVector(Input(4)) sll 11);
-    s1out(5) <= to_stdLogicVector(to_bitVector(Input(5)) sll 10);
-    s2out(5) <= to_stdLogicVector(to_bitVector(Input(5)) sll 11);
-	f1: for i in 0 to 9 generate
-        if11: if i = 0 or i = 9 generate
-            add11: adder port map (
-                A => s1out(i),
-                B => Input(i),
-                O => a2out(i)
+    add1: adder port map (
+        A => Input(0),
+        B => Input(9),
+        O => a1out(0)
+    );
+    add2: adder port map (
+        A => Input(1),
+        B => Input(8),
+        O => a1out(1)
+    );
+    add3: adder port map (
+        A => Input(2),
+        B => Input(7),
+        O => a1out(2)
+    );
+    add4: adder port map (
+        A => Input(3),
+        B => Input(6),
+        O => a1out(3)
+    );
+    add5: adder port map (
+        A => Input(4),
+        B => Input(5),
+        O => a1out(4)
+    );
+
+    s1out(0) <= to_stdLogicVector(to_bitVector(a1out(0)) sll 1);
+    s1out(1) <= to_stdLogicVector(to_bitVector(a1out(1)) sll 4);
+    s2out(1) <= to_stdLogicVector(to_bitVector(a1out(1)) sll 3);
+    s1out(2) <= to_stdLogicVector(to_bitVector(a1out(2)) sll 6);
+    s2out(2) <= to_stdLogicVector(to_bitVector(a1out(2)) sll 5);
+    s1out(3) <= to_stdLogicVector(to_bitVector(a1out(3)) sll 8);
+    s2out(3) <= to_stdLogicVector(to_bitVector(a1out(3)) sll 7);
+    s1out(4) <= to_stdLogicVector(to_bitVector(a1out(4)) sll 10);
+    s2out(4) <= to_stdLogicVector(to_bitVector(a1out(4)) sll 11);
+    -- (i0 + i9)<<1 + (i0 + i9)
+    add01: adder port map (
+        A => s1out(0),
+        B => a1out(0),
+        O => a3out(0)
+    );
+    -- (i1 + i8)<<4 + (i1 + i8)<<3 + (i1 + i8)
+    add11: adder port map (
+        A => s1out(1),
+        B => s2out(1),
+        O => a2out(1)
+    );
+    add12: adder port map (
+        A => a1out(1),
+        B => a2out(1),
+        O => a3out(1)
+    );
+    -- (i2 + i7)<<6 + (i2 + i7)<<5
+    add21: adder port map (
+        A => s1out(2),
+        B => s2out(2),
+        O => a3out(2)
+    );
+    -- (i3 + i6)<<8 + (i3 + i6)<<7
+    add32: adder port map (
+        A => s1out(3),
+        B => s2out(3),
+        O => a3out(3)
+    );
+    -- (i4 + i5)<<11 + (i4 + i5)<<10 + (i4 + i5)
+    add41: adder port map (
+        A => s1out(4),
+        B => s2out(4),
+        O => a2out(4)
+    );
+    add42: adder port map (
+        A => a1out(4),
+        B => a2out(4),
+        O => a3out(4)
+    );
+
+    f2: for i in 0 to 3 generate
+        if21: if i = 0 generate
+            add0: adder port map (
+                A => a3out(i),
+                B => a3out(i + 1),
+                O => aout(i)
             );
-		end generate if11;
-        if12: if i = 1 or i = 4 or i = 5 or i = 8 generate
-            add21: adder port map (
-                A => s1out(i),
-                B => s2out(i),
-                O => a1out(i)
+        end generate if21;
+        if22: if i > 0 generate
+            add: adder port map (
+                A => aout(i - 1),
+                B => a3out(i + 1),
+                O => aout(i)
             );
-            add22: adder port map (
-                A => a1out(i),
-                B => Input(i),
-                O => a2out(i)
-            );
-		end generate if12;
-        if13: if i = 2 or i = 3 or i = 6 or i = 7 generate
-            add31: adder port map (
-                A => s1out(i),
-                B => s2out(i),
-                O => a2out(i)
-            );
-		end generate if13;
-	end generate f1;  
---    mout(0) <= to_stdLogicVector(to_bitVector(Input(0)) sll 1 + Input(0));
---    mout(1) <= to_stdLogicVector(to_bitVector(Input(1)) sll 4 + to_bitVector(Input(1)) sll 3 + Input(1));
---    mout(2) <= to_stdLogicVector(to_bitVector(Input(2)) sll 6 + to_bitVector(Input(2)) sll 5);
---    mout(3) <= to_stdLogicVector(to_bitVector(Input(3)) sll 8 + to_bitVector(Input(3)) sll 7);
---    mout(4) <= to_stdLogicVector(to_bitVector(Input(4)) sll 11 + to_bitVector(Input(4)) sll 10 + Input(4));
---    mout(5) <= to_stdLogicVector(to_bitVector(Input(5)) sll 11 + to_bitVector(Input(5)) sll 10 + Inout(5));
---    mout(6) <= to_stdLogicVector(to_bitVector(Input(6)) sll 8 + to_bitVector(Input(6)) sll 7);
---    mout(7) <= to_stdLogicVector(to_bitVector(Input(7)) sll 6 + to_bitVector(Input(7)) sll 5);
---    mout(8) <= to_stdLogicVector(to_bitVector(Input(8)) sll 4 + to_bitVector(Input(8)) sll 3 + Input(8));
---    mout(9) <= to_stdLogicVector(to_bitVector(Input(9)) sll 1 + Input(9));
+        end generate if22;
+    end generate f2;  
 	
-	f2: for i in 0 to 8 generate
-		if21: if i = 0 generate
-			add0: adder port map (
-				A => a2out(i),
-				B => a2out(i + 1),
-				O => aout(i)
-			);
-		end generate if21;
-		if22: if i > 0 generate
-			add: adder port map (
-				A => aout(i - 1),
-				B => a2out(i + 1),
-				O => aout(i)
-			);
-		end generate if22;
-	end generate f2;  
-	
-	Output <= aout(8);
+	Output <= aout(3);
 
 		
 end Structural;
